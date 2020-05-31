0e10c347d35ae46dbd119bd238aa222f85b60c44
Zahra Nikbakht <60522280+zahranikbakht@users.noreply.github.com>
2020-05-31 03:23:47 -0400

    Update question-1.lisp

--- question-1.lisp
+++ question-1.lisp
@@ -1,24 +1,24 @@
-(defun triangle (num) 
-(cond 
-((or (not (integerp num)) (zerop num)) (print "input invalid; please enter a positive or a negative integer"))
-((< num 0)
-(dotimes (n (abs num)) 
-(dotimes (p n)
-(princ #\space))
-(dotimes (k (- (abs num) n)) 
-(write '*)
-)
-(fresh-line)
-)
-)
-(t
-(dotimes (n num) 
-(dotimes (k (- num n)) 
-(write '*) 
-)
-(fresh-line)
-)
-)
-)
- )
+(defun flattenonly (l) 
+  (cond ((null l) nil)
+        ((numberp (car l)) (cons (car l) (flattenonly (cdr l))))
+        ((listp (car l))  (append (flattenonly (car l)) (flattenonly (cdr l))))
+         (t (flattenonly (cdr l)))
+        )
+        
+        
+        )
+  
+  
+(defun remove-all (lst elt)
+(cond ((null lst) nil) ((equal(car lst) elt)(remove-all (cdr lst) elt))(t (cons (car lst) (remove-all (cdr lst) elt))))) 
 
+(defun remove2 (l)
+    (cond
+        ((null l) l)
+        ((member (car l) (cdr l)) (remove2 (cons (car l) (remove-all (cdr l) (car l)))))
+        (t (cons (car l) (remove2 (cdr l))))))
+        
+(defun flatten(l)
+
+(remove2 (flattenonly l))
+)
